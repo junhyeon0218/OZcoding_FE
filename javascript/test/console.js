b6a16b8d3255d69a2c3ecfc544d674431f510705
junhyeon0218 <junhyeon0218@daum.net>
2024-03-29 14:17:19 +0900

    ✨ Feat: js 연습용

---
+++ javascript/test/console.js
@@ -0,0 +1,13 @@
+// confirm("할거임?");
+// const a = prompt("아무거나 단어나 입력");
+// alert(`너는 ${a} 야!`);
+
+// let b;
+// console.log(b);
+// b = null;
+// console.log(b);
+// b = 3;
+// console.log(b);
+
+c = "1" + 2;
+console.log(typeof c);
