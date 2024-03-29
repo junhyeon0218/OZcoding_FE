be26d116c889cb3b323b56b33ca5db26b913718c
junhyeon0218 <junhyeon0218@daum.net>
2024-03-29 18:17:44 +0900

    ✨ Feat: test

--- javascript/test/document.js
+++ javascript/test/document.js
@@ -1,6 +1,18 @@
-const p = document.querySelector("p");
-console.log(p);
-console.log(p.textContent);
-p.textContent = "이제 p1은 이 글로 바뀝니다";
+// const p = document.querySelector("p");
+// console.log(p);
+// console.log(p.textContent);
+// p.textContent = "이제 p1은 이 글로 바뀝니다";
 
-console.log(p.textContent);
+// console.log(p.textContent);
+
+console.log(document.querySelectorAll("p"));
+
+const a = document.querySelectorAll("p");
+
+console.log(typeof a);
+
+const b = document.querySelector("h1");
+console.log(b);
+console.log(b.className); // 클래스 이름 확인
+console.log(b.style); // 이미 선언된 스타일 속성에 대한 확인
+console.log(b.title); // title 속성을 작성하면 해당 속성으로 확인 가능
