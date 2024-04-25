db14522ec07be04302cc1a07d3a9174d9c23b654
junhyeon0218 <junhyeon0218@daum.net>
2024-04-25 19:43:58 +0900

    ✨ Feat: axios 테스트, yarn 설치

---
+++ nodejs/axios/server.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+
+axios
+  .get("https://naver.com")
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
