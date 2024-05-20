c582df497d33be65aa3de2b5404bb8d5fbb4672d
junhyeon0218 <junhyeon0218@daum.net>
2024-05-20 20:40:45 +0900

    ⬇️ Docs: key수정

--- react/appleVite/src/api/axios.js
+++ react/appleVite/src/api/axios.js
@@ -1,12 +1,9 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "142bae17f83fc7ef0a4fe42c1a0ed072",
+    api_key: "",
     language: "ko-KR",
   },
 });
