ca4a13cf19a790c1699bf0f5b9aac4d3bc17294c
junhyeon0218 <junhyeon0218@daum.net>
2024-05-24 11:55:30 +0900

    ✨ Feat: react router dom 적용

--- react/appleVite/src/App.jsx
+++ react/appleVite/src/App.jsx
@@ -1,38 +1,33 @@
 import "./App.css";
-import Banner from "./components/Banner";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
-import { styled } from "styled-components";
-import Row from "./components/Row";
-import requests from "./api/requests";
+import MainPage from "../pages/MainPage";
+import LoginPage from "../pages/LoginPage";
+import DetailPage from "../pages/DetailPage";
+import SearchPage from "../pages/searchPage";
+
+const Layout = () => {
+  return (
+    <>
+      <Nav />
+      <Outlet />
+    </>
+  );
+};
 
 function App() {
   return (
     <>
-      <Container>
-        <Nav />
-        <Banner />
-        <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending} />
-        <Row title='Top Rated' id='TR' fetchUrl={requests.fetchTopRated} />
-        <Row
-          title='Action Movies'
-          id='AM'
-          fetchUrl={requests.fetchActionMovies}
-        />
-        <Row
-          title='Comedy Movies'
-          id='CM'
-          fetchUrl={requests.fetchComedyMovies}
-        />
-      </Container>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<LoginPage />} />
+          <Route path='main' element={<MainPage />} />
+          <Route path=':movieId' element={<DetailPage />} />
+          <Route path='search' element={<SearchPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
-
-const Container = styled.div`
-  position: relative;
-  display: block;
-  top: 72px;
-  padding: 0 calc(3.5vw + 5px);
-`;
 
 export default App;
