94c2da4b302dfdbad714594201fe922b42f9499f
junhyeon0218 <junhyeon0218@daum.net>
2024-06-05 21:58:52 +0900

    ✨ Feat

---
+++ type_next/next/pages/index.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <h1>홈 페이지</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ type_next/next/utils/posts.js
@@ -0,0 +1,6 @@
+export function getAllPosts() {
+  return [
+    { id: "post-1", title: "첫 번째 포스트", content: "첫 번째 포스트 내용" },
+    { id: "post-2", title: "두 번째 포스트", content: "두 번째 포스트 내용" },
+  ];
+}
