6bc883d91d4cb73ba02cf7922cfe0626494b88dd
junhyeon0218 <junhyeon0218@daum.net>
2024-06-26 22:03:49 +0900

    ✨ Feat: ts test

--- miniProject/project1/client/src/stores/index.js
+++ miniProject/project1/client/src/stores/index.js
@@ -1,8 +1,4 @@
-import {
-  configureStore,
-  combineReducers,
-  getDefaultMiddleware,
-} from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import moviesReducer from "./moviesSlice";
---
+++ typescript/test/test.js
@@ -0,0 +1,4 @@
+function hello(name) {
+    console.log("Hello, ".concat(name));
+}
+hello("world");
